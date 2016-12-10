8b52e5ad4179eafdd1da673f4c863be344ae6a4a
Tara Khanna <tmkrocks138@Taras-MacBook-Air.local>
2016-12-10 17:11:02 -0500

    compareTo was added COMPILES

--- Barcode.java
+++ Barcode.java
@@ -22,34 +22,33 @@ public class Barcode implements Comparable<Barcode>{
 //               _zip and _checkDigit are initialized.
   public Barcode(String zip) {
       if (zip.length()!=5){
-	  throw new llegalArgumentException();
+	  throw new IllegalArgumentException();
       }
       _zip = zip;
 
   }
   public String toCode(String zip){
-	if (zip.length()!=5){
-	    throw new IllegalArgumentException();
-	}
-	String code = "|";
-	try{
-	    for (int i=0; i<zip.length(); i++){
-		code+=bar[(int)zip.charAt(i)];
-	    }
-	}
-	catch{
-	  throw new IllegalArgumentException("Invalid Zip");
+    if (zip.length()!=5){	  
+	 throw new IllegalArgumentException();
+    }
+    String code = "|"; 
+    try{
+	for (int i=0; i<zip.length(); i++){
+	    code+=bar[(int)zip.charAt(i)];
 	}
     }
-
-/*
-// postcondition: Creates a copy of a bar code.
-  public Barcode clone(){
-      Barcode x = new Barcode(this._zip);
-      //      x = this;
-      return this;
+    catch(IllegalArgumentException e){
+         System.out.println("Invalid Zip");
+    }
+    return code +"|";
   }
 
+ public int compareTo(Barcode other){
+    int x = this.compareTo(other);
+    return x;
+ }
+/*
+// postcondition: Creates a copy of a bar code.
 
 // postcondition: computes and returns the check sum for _zip
   private int checkSum(){}
